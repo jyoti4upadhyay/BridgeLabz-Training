f057ad3fae3eb119a19172f3ace8780b0f401bdd
Jyoti Upadhyay <jyoti.upadhyay_cs22@gla.ac.in>
2025-12-18 12:19:02 +0530

    NEW ADDED

--- core-java-practice/gcr-codebase/programming-elements/HarryAge.java
+++ core-java-practice/gcr-codebase/programming-elements/HarryAge.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class HarryAge {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-    int birth_year= sc.nextInt();
-    int current_year=sc.nextInt();
-    int age=current_year-birth_year;
+        int birth_year= sc.nextInt();
+        int current_year=sc.nextInt();
+        int age=current_year-birth_year;
 
     System.out.println("Age" + age);
 
