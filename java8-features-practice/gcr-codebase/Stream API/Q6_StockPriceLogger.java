7e5429e4f128454ecac1309deecc8119f53709c4
Jyoti Upadhyay <jyoti.upadhyay_cs22@gla.ac.in>
2026-02-07 15:38:27 +0530

    [Jyoti] added collectors

--- java8-features-practice/gcr-codebase/Stream API/Q6_StockPriceLogger.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class Q6_StockPriceLogger {
-    public static void main(String[] args) {
-        List<Double> prices = Arrays.asList(101.5, 102.3, 99.8, 105.0);
-        prices.forEach(price -> System.out.println("Stock Price: " + price));
-    }
-}
-
